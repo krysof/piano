14273f9f111fd4e5f0e3fdc9e62129f5844ab445
白河愁 <kryso@21cn.com>
2026-08-07 18:16:28 +0900

    Publish v20260807.10

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const $ = (id) => document.getElementById(id);
-const ASSET_VERSION = 'reset-20260807-09';
+const ASSET_VERSION = 'reset-20260807-10';
 const SONG_CATALOG = Object.freeze([
   ...Array.from(window.FreezaSongCatalog || []),
   ...Array.from(window.FreezaVaultSongCatalog || []),
